196906777f7ca90cc45044aae416004cc8a6449e
brassardfelix <brassardfelix@gmail.com>
2020-10-28 15:55:24 -0400

    correction1-code.version

---
+++ MakeCacheMatrix.R
@@ -0,0 +1,13 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(inverse) i <<- inverse
+  get_inverse <- function() i
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
+}
